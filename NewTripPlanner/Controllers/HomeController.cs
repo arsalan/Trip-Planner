6d8ea1171ce373cbcdd34af95b739f6d7f7d9c37
unknown <Arsalan@Arsalan-W520.(none)>
2011-11-01 21:55:25 -0400

    Upgraded to razor - NewTripPlanner

---
+++ NewTripPlanner/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TripPlanner.Controllers
+{
+    [HandleError]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewData["Message"] = "Welcome to Your Trip Planner!";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult TripPlan()
+        {
+            return View("Planner_WorkInProgress");
+        }
+    }
+}
